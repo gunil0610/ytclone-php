b69473c62f832766ec270336e74fe3a1bdd3abcf
Gunil <gunil.na1@gmail.com>
2020-11-30 23:08:29 +0100

    project setup

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>VideoTube</title>
+</head>
+<body>
+    <div id="pageContainer">
+        <div id="mastHeadContainer">
+            Hello
+        </div>
+
+        <div id="sideNavContainer">
+
+        </div>
+
+        <div id="mainSectionContainer">
+            <div id="mainContentContainer">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
