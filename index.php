130d5963bb9d22b9b1349af864572609a9c08fa5
Gunil <gunil.na1@gmail.com>
2020-12-01 00:13:49 +0100

    writing codes to hide left nav bar"

--- index.php
+++ index.php
@@ -3,24 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    
     <title>VideoTube</title>
 </head>
 <body>
     <div id="pageContainer">
         <div id="mastHeadContainer">
-            Hello
+            <button class="navShowHide btn btn-lg btn-dark">Click Me</button>
         </div>
 
-        <div id="sideNavContainer">
+        <!-- <div id="sideNavContainer" class="hidden"> -->
+        <div id="sideNavContainer" style="display:none;">
 
         </div>
 
         <div id="mainSectionContainer">
+
             <div id="mainContentContainer">
-                
+                hello everyone
             </div>
         </div>
     </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="assets/js/commonActions.js"></script>
 </body>
 </html>
