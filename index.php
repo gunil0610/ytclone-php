a705a2d239162bdd21cbc95ece422b3a1fe2c023
Gunil <gunil.na1@gmail.com>
2020-12-07 23:36:45 +0100

    add images and add logo and search bar on header

--- index.php
+++ index.php
@@ -5,27 +5,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     
     <title>VideoTube</title>
 </head>
 <body>
     <div id="pageContainer">
+
         <div id="mastHeadContainer">
-            <button class="navShowHide btn btn-lg btn-dark">Click Me</button>
+            <button class="navShowHide">
+                <img src="assets/images/icons/menu.png">
+            </button>
+
+            <a class="logoContainer" href="index.php">
+                <img src="assets/images/icons/VideoTubeLogo.png" title="logo" alt="Site logo">
+            </a>
+
+            <div class="searchBarContainer">
+                <form action="search.php" method="GET">
+                    <input type="text" class="searchBar" name="term" placeholder="Search...">
+                    <button class="searchButton">
+                        <img src="assets/images/icons/search.png">
+                    </button>
+                </form>
+            </div>
         </div>
+
 
         <!-- <div id="sideNavContainer" class="hidden"> -->
         <div id="sideNavContainer" style="display:none;">
-
+        
         </div>
 
         <div id="mainSectionContainer">
-
+        
             <div id="mainContentContainer">
                 hello everyone
             </div>
+
         </div>
+
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
