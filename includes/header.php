83cc6feb3a8718d3b79db19c954ec5ebc5410924
Gunil <gunil.na1@gmail.com>
2020-12-07 23:56:16 +0100

    finishing header and separating header and footer php file

---
+++ includes/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+    <title>VideoTube</title>
+</head>
+<body>
+    <div id="pageContainer">
+
+        <div id="mastHeadContainer">
+            <button class="navShowHide">
+                <img src="assets/images/icons/menu.png">
+            </button>
+
+            <a class="logoContainer" href="index.php">
+                <img src="assets/images/icons/VideoTubeLogo.png" title="logo" alt="Site logo">
+            </a>
+
+            <div class="searchBarContainer">
+                <form action="search.php" method="GET">
+                    <input type="text" class="searchBar" name="term" placeholder="Search...">
+                    <button class="searchButton">
+                        <img src="assets/images/icons/search.png">
+                    </button>
+                </form>
+            </div>
+
+            <div class="rightIcons">
+                <a href="upload.php">
+                    <img src="assets/images/icons/upload.png" alt="upload">
+                </a>
+
+                <a href="#">
+                    <img src="assets/images/profilePictures/default.png" alt="profile pic">
+                </a>
+            </div>
+        </div>
+
+
+        <!-- <div id="sideNavContainer" class="hidden"> -->
+        <div id="sideNavContainer" style="display:none;">
+        
+        </div>
+
+        <div id="mainSectionContainer">
+        
+            <div id="mainContentContainer">
--- index.php
+++ index.php
@@ -1,54 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-    
-    <title>VideoTube</title>
-</head>
-<body>
-    <div id="pageContainer">
+<?php require_once("includes/header.php"); ?>
 
-        <div id="mastHeadContainer">
-            <button class="navShowHide">
-                <img src="assets/images/icons/menu.png">
-            </button>
+hello everyone
 
-            <a class="logoContainer" href="index.php">
-                <img src="assets/images/icons/VideoTubeLogo.png" title="logo" alt="Site logo">
-            </a>
-
-            <div class="searchBarContainer">
-                <form action="search.php" method="GET">
-                    <input type="text" class="searchBar" name="term" placeholder="Search...">
-                    <button class="searchButton">
-                        <img src="assets/images/icons/search.png">
-                    </button>
-                </form>
-            </div>
-        </div>
-
-
-        <!-- <div id="sideNavContainer" class="hidden"> -->
-        <div id="sideNavContainer" style="display:none;">
-        
-        </div>
-
-        <div id="mainSectionContainer">
-        
-            <div id="mainContentContainer">
-                hello everyone
-            </div>
-
-        </div>
-
-    </div>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    <script src="assets/js/commonActions.js"></script>
-</body>
-</html>
+<?php require_once("includes/footer.php"); ?>
